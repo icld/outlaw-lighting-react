6e5aaec67118e5aa8f80a8089ffa4b4c85518055
icld <ian@icldesign.com>
2021-06-03 21:22:28 -0700

    add h1 to landing

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -20,25 +20,8 @@ const Carousel = ({ images }) => {
       <img
         src={images[activeImage].image}
         alt="concert"
-        className="w-full transition-opacity duration-500 ease-in-out opacity-50"
+        className="w-full transition-opacity duration-500 ease-in-out opacity-90"
       />
-      {/* <div className="carousel-smaller">
-        {images.map((photo, index) => (
-          <img
-            key={photo.image}
-            src={photo.image}
-            data-index={index}
-            className={index === activeImage ? "activeImage" : ""}
-            alt="concert"
-          />
-        ))}
-      </div> */}
-
-      {/* <img
-        src={images[0].image}
-        alt="event"
-        className="rounded-xl w-full"
-      ></img> */}
     </div>
   );
 };
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -13,6 +13,12 @@ const Landing = () => {
         className="mx-auto z-10 -my-28 w-screen absolute"
       ></img>
       <Carousel images={images} className="" />
+      <h1 className="text-indigo-600 sm:text-4xl lg:text-5xl font-extrabold mx-8 mt-6">
+        Event Production Solutions
+      </h1>
+      <h1 className=" sm:text-3xl lg:text-4xl text-indigo-200 font-extrabold mx-8 mt-4">
+        From Design to Activation
+      </h1>
     </div>
   );
 };
