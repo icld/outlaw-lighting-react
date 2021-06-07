d8d6ba31c940f7945fc789c4bf1102ef840e50dd
icld <ian@icldesign.com>
2021-06-07 14:13:05 -0700

    install aspect ratio plugin.  add aspect ratio to DetailsCard

--- src/components/GearCategories/DetailsCard/DetailsCard.js
+++ src/components/GearCategories/DetailsCard/DetailsCard.js
@@ -6,15 +6,15 @@ const DetailsCard = (props) => {
   return (
     <div>
       return (
-      <div className="my-10  w-4/6 group h-auto m-auto rounded-xl bg-white relative transition-all duration-300 ring-4 ring-blue-500 justify-center items-center flex flex-col">
+      <div className="my-10 w-5/6 group h-auto m-auto rounded-xl bg-white relative transition-all duration-300 ring-4 ring-blue-500 justify-center items-center flex flex-col">
         <h2 className="   text-6xl mx-14 my-14 "> {item.name}</h2>
 
-        <div className="grid  grid-cols-1 lg:grid-cols-2  ">
-          <div className="mx-14  h-full shadow-xl transition-all duration-300 overflow-hidden flex items-center justify-center ">
+        <div className="grid  grid-cols-1 md:grid-cols-1 lg:grid-cols-2  ">
+          <div className="ml-14 mr-7 h-full shadow-xl transition-all duration-300 overflow-hidden flex items-center justify-center ">
             <img src={item.image} alt="lighting fixture" className="  w-full" />
           </div>
-          <div className="   flex items-center justify-center">
-            <ul className="mx-auto prose-2xl  list-disc  w-11/12  align-middle">
+          <div className="    items-center ">
+            <ul className="mr-14 ml-7 p-14 border-transparent shadow-lg prose-xl  list-disc  align-middle">
               {item.specs.map((spec) => (
                 <li className=" list-item">{spec}</li>
               ))}
@@ -22,22 +22,24 @@ const DetailsCard = (props) => {
           </div>
         </div>
 
-        <p className=" text-xl mx-14 my-14 m-auto text-justify ">{item.info}</p>
+        <div className=" ">
+          <p className=" leading-relaxed text-2xl mx-14 my-14 m-auto text-justify ">
+            {item.info}
+          </p>
 
-        {item.vid ? (
-          <div>
-            <iframe
-              width="560"
-              height="315"
-              src={item.vid}
-              title="YouTube video player"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
-              className="m-auto mb-10 object-cover shadow-2xl"
-            ></iframe>
-          </div>
-        ) : null}
+          {item.vid ? (
+            <div className=" w-full  aspect-w-16 aspect-h-9">
+              <iframe
+                src={item.vid}
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+                className="object-fill "
+              ></iframe>
+            </div>
+          ) : null}
+        </div>
       </div>
       );
     </div>
