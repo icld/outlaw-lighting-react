8f18129ac5f530b5e3f82dfe7416247b51d1ce91
icld <ian@icldesign.com>
2021-06-07 15:42:02 -0700

    breakpoints for detailscard

--- src/components/GearCategories/DetailsCard/DetailsCard.js
+++ src/components/GearCategories/DetailsCard/DetailsCard.js
@@ -6,15 +6,15 @@ const DetailsCard = (props) => {
   return (
     <div>
       return (
-      <div className="my-10 w-5/6 group h-auto m-auto rounded-xl bg-white relative transition-all duration-300 ring-4 ring-blue-500 justify-center items-center flex flex-col">
-        <h2 className="   text-6xl mx-14 my-14 "> {item.name}</h2>
+      <div className="my-10 sm:mx-4 lg:m-auto lg:w-11/12 group h-auto rounded-xl bg-white relative transition-all duration-300 ring-4 ring-blue-500 justify-center items-center flex flex-col">
+        <h2 className="   text-6xl mx-14 my-14 ">{item.name}</h2>
 
-        <div className="grid  grid-cols-1 md:grid-cols-1 lg:grid-cols-2  ">
-          <div className="ml-14 mr-7 h-full shadow-xl transition-all duration-300 overflow-hidden flex items-center justify-center ">
-            <img src={item.image} alt="lighting fixture" className="  w-full" />
+        <div className="grid  grid-cols-1 md:grid-cols-1 lg:grid-cols-2  items-center">
+          <div className="lg:ml-14 lg:mr-7 h-full shadow-xl transition-all duration-300 overflow-hidden flex items-center justify-center ">
+            <img src={item.image} alt="lighting fixture" className="w-full" />
           </div>
           <div className="    items-center ">
-            <ul className="mr-14 ml-7 p-14 border-transparent shadow-lg prose-xl  list-disc  align-middle">
+            <ul className="lg:mr-14 lg:ml-7 p-14 border-transparent shadow-lg prose-xl  list-disc  align-middle">
               {item.specs.map((spec) => (
                 <li className=" list-item">{spec}</li>
               ))}
@@ -28,7 +28,7 @@ const DetailsCard = (props) => {
           </p>
 
           {item.vid ? (
-            <div className=" w-full  aspect-w-16 aspect-h-9">
+            <div className=" w-full aspect-w-16 aspect-h-9">
               <iframe
                 src={item.vid}
                 title="YouTube video player"
