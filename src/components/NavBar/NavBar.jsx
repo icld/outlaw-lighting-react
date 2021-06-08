51990dd243f210c12312b14c896ae04ca4bf3acc
icld <ian@icldesign.com>
2021-06-08 16:36:17 -0700

    add 3d file

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,7 +3,8 @@ import { NavLink } from "react-router-dom";
 import { Fragment } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
-import longLogo from "../../assets/";
+import Logo from "../../assets/outlaw-logo-long-text.png";
+import logo3d from "../../assets/outlaw-3d-tests.png";
 
 const navigation = [
   { name: "Design", href: "design" },
@@ -21,21 +22,21 @@ const Nav = () => {
           <Fragment>
             <div className="relative pt-8 px-4 sm:px-6 lg:px-8 ">
               <nav
-                className="relative flex items-center justify-between sm:h-10 lg:justify-start pb-8"
+                className="relative flex items-center justify-between h-full lg:justify-start pb-8"
                 aria-label="Global"
               >
                 <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-                  <div className="flex items-center justify-between w-full md:w-auto">
-                    <NavLink to="/">
+                  <div className="flex items-center justify-between h-full w-full md:w-auto text-xl">
+                    <NavLink to="/" className="w-52">
                       <img
-                        className="h-8 w-auto sm:h-10"
-                        src={Logo}
+                        className="h-auto object-cover "
+                        src={logo3d}
                         alt="logo"
                       />
                     </NavLink>
                     <a
                       href="tel:+19712355608"
-                      className="font-medium text-indigo-400 hover:text-indigo-500 lg:float-right"
+                      className="md:hidden ml-4 mr-4 font-medium text-indigo-400 hover:text-indigo-500 lg:float-right"
                     >
                       1-900-Outlaw1
                     </a>
@@ -52,7 +53,7 @@ const Nav = () => {
                     <NavLink
                       key={item.name}
                       to={`/${item.href}`}
-                      className="font-medium text-gray-500 hover:text-gray-300"
+                      className="font-medium text-2xl text-gray-500 hover:text-gray-300"
                     >
                       {item.name}
                     </NavLink>
@@ -74,7 +75,7 @@ const Nav = () => {
               <Popover.Panel
                 focus
                 static
-                className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+                className="absolute ml-10 mr-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden z-20"
               >
                 <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
                   <div className="px-5 pt-4 flex items-center justify-between">
@@ -82,7 +83,7 @@ const Nav = () => {
                       <img className="h-8 w-auto" src={Logo} alt="" />
                     </div>
                     <div className="-mr-2">
-                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 z-2">
                         <span className="sr-only">Close main menu</span>
                         <XIcon className="h-6 w-6" aria-hidden="true" />
                       </Popover.Button>
@@ -90,13 +91,13 @@ const Nav = () => {
                   </div>
                   <div className="px-2 pt-2 pb-3 space-y-1">
                     {navigation.map((item) => (
-                      <a
+                      <NavLink
                         key={item.name}
-                        href={item.href}
-                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                        to={item.href}
+                        className="block px-3 py-2 rounded-md  font-medium text-xl text-gray-700 hover:text-gray-900 hover:bg-gray-50"
                       >
                         {item.name}
-                      </a>
+                      </NavLink>
                     ))}
                   </div>
                   <a
