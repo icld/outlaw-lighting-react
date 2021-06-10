8bd84908d90c382d3a592ac5b13905a303d0c7cc
icld <ian@icldesign.com>
2021-06-09 17:33:06 -0700

    fix small problem for deploy

--- src/components/Design/Design.jsx
+++ src/components/Design/Design.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 const Design = () => {
-  return <div className="h-screen bg-white grid grid"></div>;
+  return <div className="h-screen bg-white grid "></div>;
 };
 
 export default Design;
