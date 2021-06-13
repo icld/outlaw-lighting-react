4eb522e794652ad291482d459aba15a0df50a59c
icld <ian@icldesign.com>
2021-06-13 14:30:52 -0700

    add design cards.  Need to fix card overlap layout

---
+++ src/components/Design/DesignData.js
@@ -0,0 +1,25 @@
+import design1 from "./DesignAssets/design1.png";
+import design2 from "./DesignAssets/design1.png";
+
+const designData = [
+  {
+    title: "Some text about WZYWIG HERE",
+    subTitle: "sub title",
+    body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi distinctio et ducimus hic dolor nemo cum omnis quae corporis adipisci assumenda, maiores dolorem eveniet nam perferendis cumque",
+    img1: design1,
+    img2: "",
+    alt: "design",
+    button: "do something",
+  },
+  {
+    title: "Some text about Vectorworks HERE",
+    subTitle: "sub title",
+    body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi distinctio et ducimus hic dolor nemo cum omnis quae corporis adipisci assumenda, maiores dolorem eveniet nam perferendis cumque",
+    img1: design2,
+    img2: "",
+    alt: "design",
+    button: "do something",
+  },
+];
+
+export default designData;
