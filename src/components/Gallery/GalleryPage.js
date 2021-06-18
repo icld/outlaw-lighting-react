e78f4157f28f9c616f96491b9d7c2cdf13bb0ae6
icld <ian@icldesign.com>
2021-06-17 20:01:55 -0700

    add gallery photos and routing

--- src/components/Gallery/GalleryPage.js
+++ src/components/Gallery/GalleryPage.js
@@ -1,7 +1,32 @@
+import Images from "./GalleryImages";
+
 const GalleryPage = (props) => {
+  console.log(props.item);
   return (
     <div className=" h-full bg-white">
-      <h1 className="text-9xl text-black"> looking for {props.item}</h1>{" "}
+      <h1 className="text-9xl text-black"> looking for {props.item}</h1>
+      <div>
+        { return !props.item <div>Find Something </div> :
+      pro
+
+        }
+
+        }
+        {/* {Images.concert.map((category) => {
+          return console.log(category.img);
+          // if (category === props.item) {
+          //   return category.map((image) => {
+          //     return (
+          //       <div>
+          //         <img src={image.img} alt="concert" key={image.id} />
+          //       </div>
+          //     );
+          //   });
+          // } else {
+          //   return null;
+          // }
+        })} */}
+      </div>
     </div>
   );
 };
